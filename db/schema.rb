1984dc0f6df99d2ab1b957b30836f797d8a30d34
PSalmers <patricksalmers@gmail.com>
2017-03-24 19:33:22 -0400

    added invitations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324182412) do
+ActiveRecord::Schema.define(version: 20170324232836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 20170324182412) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.string   "email"
+    t.string   "token"
+    t.integer  "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_invitations_on_room_id", using: :btree
   end
 
   create_table "memberships", force: :cascade do |t|
