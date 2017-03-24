9041e8c9c9f2ace53f800872ca0f26d8ea9a5786
PSalmers <patricksalmers@gmail.com>
2017-03-24 17:36:17 -0400

    more routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,27 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def rooms_for_user
+    person = Person.find(params[:id])
+    render json: Room.where(person: person)
+  end
+
+  def rooms
     render json: Room.all
+  end
+
+  def join_room
+    Membership.create!(
+                  person: Person.find(params[:id]),
+                  room: Room.find(params[:room_id])
+    )
+  end
+
+  def create_post
+    Post.create!(
+            person: Person.find(params[:id]),
+            room: Room.find(params[:room_id]),
+            body: params[:body]
+    )
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  match 'registry/user' => 'application#rooms_for_user', via: [:get]
+  match 'registry/user' => 'application#rooms', via: [:get]
+  match 'registry/user/:id' => 'application#rooms_for_user', via: [:get]
+  match 'registry/subscribe' => 'application#join_room', via: [:post]
+
+  match 'posts/create' => 'application#create_post', via: [:post]
 end
