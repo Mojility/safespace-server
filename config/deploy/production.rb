329a06e778528b18d0df88f64dd5fda4d939f699
Stacey Vetzal <stacey@vetzal.com>
2017-03-24 22:50:23 -0400

    updating for more production config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ server 'safespace.commonsroot.com', user: 'safespace', roles: %w{web app db}
 set :rails_env, :production
 
 set :branch, :master
-set :deploy_to, "/app/safespace/#{fetch(:application)}"
+set :deploy_to, "/home/safespace/#{fetch(:application)}"
